14ae00c6ce7b542198b9ec8bfce349040b8fd937
Hai Ren <hren@coupang.com>
2017-08-30 14:39:41 -0700

    [000] download function

--- src/main/java/com/renhai/manage/ui/MainViewController.java
+++ src/main/java/com/renhai/manage/ui/MainViewController.java
@@ -18,22 +18,31 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.ChoiceBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.converter.DateStringConverter;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.IntegerStringConverter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.MethodUtils;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalInt;
+import java.lang.reflect.InvocationTargetException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -43,6 +52,8 @@ import java.util.stream.IntStream;
 @Component
 @Slf4j
 public class MainViewController {
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMddHHmmss");
 
     @FXML
     public TextField filterField;
@@ -75,48 +86,30 @@ public class MainViewController {
             dataTable.getColumns().addAll(column);
         }
 
-        // 1. Wrap the ObservableList in a FilteredList (initially display all data).
         FilteredList<TesterDto> filteredData = new FilteredList<>(this.data, p -> true);
 
-        // 2. Set the filter Predicate whenever the filter changes.
-        filterField.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredData.setPredicate(dto -> {
-                // If filter text is empty, display all persons.
-                if (StringUtils.isBlank(newValue)) {
-                    return true;
-                }
+        filterField.textProperty().addListener((observable, oldValue, newValue) -> filteredData.setPredicate(dto -> {
+			if (StringUtils.isBlank(newValue)) {
+				return true;
+			}
 
-                // Compare first name and last name of every person with filter text.
-                String lowerCaseFilter = newValue.toLowerCase();
-//                if (dto.getName().toLowerCase().contains(lowerCaseFilter)) {
-//                    return true; // Filter matches first name.
-//                } else if (dto.getAccount().toLowerCase().contains(lowerCaseFilter)) {
-//                    return true; // Filter matches last name.
-//                }
-                for (ColumnEnum columnEnum : ColumnEnum.values()) {
-                    try {
-                        Object value  = MethodUtils.invokeMethod(dto, "get"+ StringUtils.capitalize(columnEnum.name()));
-                        if (value != null && value.toString().contains(lowerCaseFilter)) {
-                            return true;
-                        }
-                    } catch (Exception e) {
-                        log.error(e.getMessage(), e);
-                    }
+			String lowerCaseFilter = newValue.toLowerCase();
+			for (ColumnEnum columnEnum : ColumnEnum.values()) {
+				try {
+					Object value  = MethodUtils.invokeMethod(dto, "get"+ StringUtils.capitalize(columnEnum.name()));
+					if (value != null && value.toString().contains(lowerCaseFilter)) {
+						return true;
+					}
+				} catch (Exception e) {
+					log.error(e.getMessage(), e);
+				}
 
-                }
-                return false; // Does not match.
-            });
-        });
-
-        // 3. Wrap the FilteredList in a SortedList.
+			}
+			return false;
+		}));
         SortedList<TesterDto> sortedData = new SortedList<>(filteredData);
-
-        // 4. Bind the SortedList comparator to the TableView comparator.
         sortedData.comparatorProperty().bind(dataTable.comparatorProperty());
-
-        // 5. Add sorted (and filtered) data to the table.
         dataTable.setItems(sortedData);
-
     }
 
     private void initData() {
@@ -183,6 +176,7 @@ public class MainViewController {
 
     private TableColumn generateColumn(ColumnEnum columnEnum) {
         TableColumn column = new TableColumn<>(columnEnum.getDisplayName());
+        column.setId(columnEnum.name());
         column.setCellValueFactory(new PropertyValueFactory(columnEnum.name()));
         column.setOnEditCommit(event -> this.updateField(columnEnum, event));
 
@@ -232,5 +226,89 @@ public class MainViewController {
             }
         }
         return column;
+    }
+
+    @FXML
+    public void handleUpload(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void handleDownload(ActionEvent actionEvent) {
+        File excelFile = generateTempFile();
+        if (excelFile == null) {
+            log.warn("generate excel file failed.");
+            return;
+        }
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Export File");
+        fileChooser.setInitialFileName(String.format("export-%s.xlsx", DATE_FORMAT.format(new Date())));
+        File destFile = fileChooser.showSaveDialog(null);
+        if (destFile != null) {
+            log.info(destFile.getAbsolutePath());
+            try {
+                FileCopyUtils.copy(excelFile, destFile);
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+    }
+
+    private String[] getVisibleFields() {
+        List<String> visibleFields = this.dataTable.getColumns().stream().filter(column -> column.isVisible()).map(column -> column.getId()).collect(Collectors.toList());
+        return visibleFields.toArray(new String[visibleFields.size()]);
+    }
+
+    private File generateTempFile() {
+        try {
+            List<TesterDto> exportData;
+            ObservableList<TesterDto> selectedRows = dataTable.getSelectionModel().getSelectedItems();
+            if (CollectionUtils.isEmpty(selectedRows)) {
+                exportData = testerService.getAllTesters();
+            } else {
+                exportData = new ArrayList<>(selectedRows);
+            }
+
+            String[] fields = getVisibleFields();
+            Workbook excel = createExcel(fields, exportData);
+            File tempFile = File.createTempFile(RandomStringUtils.randomAlphabetic(8), "xlsx");
+            tempFile.deleteOnExit();
+            FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
+            excel.write(fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            return tempFile;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return null;
+        }
+    }
+
+    private Workbook createExcel(String[] fields, List<TesterDto> testers) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Sheet1");
+        Row header = sheet.createRow(0);
+        for (int i = 0; i < fields.length; i ++) {
+            org.apache.poi.ss.usermodel.Cell cell = header.createCell(i);
+            ColumnEnum columnEnum = ColumnEnum.fromName(fields[i]);
+            cell.setCellValue(columnEnum.getDisplayName());
+        }
+
+        int rowNum = 1;
+        for (TesterDto tester : testers) {
+            Row row = sheet.createRow(rowNum ++);
+            int colNum = 0;
+            for (String fieldName : fields) {
+                Object value = MethodUtils.invokeMethod(tester, "get"+ org.apache.commons.lang3.StringUtils.capitalize(fieldName));
+                if (value == null) {
+                    row.createCell(colNum ++).setCellValue(org.apache.commons.lang3.StringUtils.EMPTY);
+                } else {
+                    row.createCell(colNum ++).setCellValue(value.toString());
+                }
+            }
+        }
+
+        return workbook;
     }
 }
